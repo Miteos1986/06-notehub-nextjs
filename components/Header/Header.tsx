2698953fb84f6b12cff3bf889245349b15d6c4b7
smelik81 <artem.smelyanskiy81@gmail.com>
2026-01-12 11:30:06 +0200

    created components und create env

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import css from "./Header.module.css";
+
+function Header() {
+  return (
+    <div>
+      <h2>Header</h2>
+    </div>
+  );
+}
+
+export default Header;
---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import css from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.getElementById("modal-root")!;
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal = ({ children, onClose }: ModalProps) => {
+  const handleBackdropClose = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    const handleEscClose = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscClose);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscClose);
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClose}
+    >
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+export default Modal;
